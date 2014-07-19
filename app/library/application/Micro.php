83abc053389680860153f8451bf566385caa198f
Nikola Derikonjic <nikola@pregmatch.org>
2014-07-19 21:40:33 +0200

    First commit

---
+++ app/library/application/Micro.php
@@ -0,0 +1,192 @@
+<?php
+
+/**
+ * Small Micro application to run simple/rest based applications
+ *
+ * @package Application
+ * @author Jete O'Keeffe
+ * @version 1.0
+ * @link http://docs.phalconphp.com/en/latest/reference/micro.html
+ * @example
+ 	$app = new Micro();
+	$app->setConfig('/path/to/config.php');
+	$app->setAutoload('/path/to/autoload.php');
+	$app->get('/api/looks/1', function() { echo "Hi"; });
+	$app->finish(function() { echo "Finished"; });
+	$app->run();
+ */
+
+namespace Application;
+
+use Interfaces\IRun as IRun;
+
+class Micro extends \Phalcon\Mvc\Micro implements IRun {
+
+    /**
+     * Pages that doesn't require authentication
+     * @var array
+     */
+    protected $_noAuthPages;
+
+	/**
+	 * Constructor of the App
+	 */
+	public function __construct() {
+        $this->_noAuthPages = array();
+	}
+
+	/**
+	 * Set Dependency Injector with configuration variables
+	 *
+	 * @throws Exception		on bad database adapter
+	 * @param string $file		full path to configuration file
+	 */
+	public function setConfig($file) {
+		if (!file_exists($file)) {
+			throw new \Exception('Unable to load configuration file');
+		}
+
+		$di = new \Phalcon\DI\FactoryDefault();
+		$di->set('config', new \Phalcon\Config(require $file));
+
+		$di->set('db', function() use ($di) {
+			$type = strtolower($di->get('config')->database->adapter);
+			$creds = array(
+				'host' => $di->get('config')->database->host,
+				'username' => $di->get('config')->database->username,
+				'password' => $di->get('config')->database->password,
+				'dbname' => $di->get('config')->database->name
+			);
+
+			if ($type == 'mysql') {
+				$connection =  new \Phalcon\Db\Adapter\Pdo\Mysql($creds);
+			} else if ($type == 'postgres') {
+				$connection =  new \Phalcon\Db\Adapter\Pdo\Postgesql($creds);
+			} else if ($type == 'sqlite') {
+				$connection =  new \Phalcon\Db\Adapter\Pdo\Sqlite($creds);
+			} else {
+				throw new Exception('Bad Database Adapter');
+			}
+
+			return $connection;
+		});
+
+		$this->setDI($di);
+	}
+
+    public function getOauth2Config($file){
+        if (!file_exists($file)) {
+            throw new \Exception('Unable to load configuration file');
+        }
+
+        $di = new \Phalcon\DI\FactoryDefault();
+        $di->set('config', new \Phalcon\Config(require $file));
+
+        return $di->get("config")->oauth2;
+    }
+	/**
+	 * Set namespaces to tranverse through in the autoloader
+	 *
+	 * @link http://docs.phalconphp.com/en/latest/reference/loader.html
+	 * @throws Exception
+	 * @param string $file		map of namespace to directories
+	 */
+	public function setAutoload($file, $dir, $vendorDir) {
+		if (!file_exists($file)) {
+			throw new \Exception('Unable to load autoloader file');
+		}
+
+		// Set dir to be used inside include file
+		$namespaces = include $file;
+
+		$loader = new \Phalcon\Loader();
+		$loader->registerNamespaces($namespaces)->register();
+
+
+        /**
+         * We're a registering a set of directories taken from the configuration file
+         */
+        $loader->registerDirs(
+            array(
+                $vendorDir."/Oauth2/src"
+            )
+        )->register();
+
+    }
+
+	/**
+	 * Set Routes\Handlers for the application
+	 *
+	 * @throws Exception
+	 * @param file			file thats array of routes to load
+	 */
+	public function setRoutes($file) {
+		if (!file_exists($file)) {
+			throw new \Exception('Unable to load routes file');
+		}
+
+		$routes = include($file);
+
+		if (!empty($routes)) {
+			foreach($routes as $obj) {
+
+				switch($obj['method']) {
+					case 'get':
+						$this->get($obj['route'], $obj['handler']);
+						break;
+					case 'post':
+						$this->post($obj['route'], $obj['handler']);
+						break;
+					case 'delete':
+						$this->delete($obj['route'], $obj['handler']);
+						break;
+					case 'put':
+						$this->head($obj['route'], $obj['handler']);
+						break;
+					case 'options':
+						$this->options($obj['route'], $obj['handler']);
+						break;
+					case 'patch':
+						$this->patch($obj['route'], $obj['handler']);
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Set events to be triggered before/after certain stages in Micro App
+	 *
+	 * @param object $event		events to add
+	 */
+	public function setEvents(\Phalcon\Events\Manager $events) {
+		$this->setEventsManager($events);
+	}
+
+    /**
+     *
+     */
+    public function getUnauthenticated() {
+        return $this->_noAuthPages;
+    }
+	/**
+	 * Main run block that executes the micro application
+	 *
+	 */
+	public function run() {
+
+		// Handle any routes not found
+		$this->notFound(function () {
+			$response = new \Phalcon\Http\Response();
+			$response->setStatusCode(404, 'Not Found')->sendHeaders();
+			$response->setContent('Page doesn\'t exist.');
+			$response->send();
+		});
+
+		$this->handle();
+
+	}
+
+}
---
+++ public/index.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * Driver for PHP HMAC Restful API using PhalconPHP's Micro framework
+ * 
+ * @package None
+ * @author  Jete O'Keeffe 
+ * @license none
+ */
+
+
+// Setup configuration files
+$dir = dirname(__DIR__);
+$appDir = $dir . '/app';
+$vendorDir = dirname(dirname(__FILE__))."/vendor";
+
+// Necessary requires to get things going
+require $appDir . '/library/utilities/debug/PhpError.php';
+require $appDir . '/library/interfaces/IRun.php';
+require $appDir . '/library/application/Micro.php';
+
+// Capture runtime errors
+register_shutdown_function(['Utilities\Debug\PhpError','runtimeShutdown']);
+
+// Necessary paths to autoload & config settings
+$configPath = $appDir . '/config/';
+$config = $configPath . 'config.php';
+$autoLoad = $configPath . 'autoload.php';
+$routes = $configPath . 'routes.php';
+
+/*
+     * auto load vendor
+     */
+require __DIR__ ."/../vendor/autoload.php";
+
+use \Models\Api as Api;
+
+try {
+	$app = new Application\Micro();
+
+	// Record any php warnings/errors
+	set_error_handler(['Utilities\Debug\PhpError','errorHandler']);
+
+	// Setup App (dependency injector, configuration variables and autoloading resources/classes)
+	$app->setAutoload($autoLoad, $appDir, $vendordir);
+	$app->setConfig($config);
+
+
+    $oauthConfig = $app->getOauth2Config($config);
+
+
+	// Setup HMAC Authentication callback to validate user before routing message
+	// Failure to validate will stop the process before going to proper Restful Route
+	$app->setEvents(new \Events\Api\HmacAuthenticate($oauthConfig));
+
+	// Setup RESTful Routes
+	$app->setRoutes($routes);
+
+    $app->setService('oauth2', function() use ($oauthConfig) {
+
+
+        $oauthdb = new Phalcon\Db\Adapter\Pdo\Mysql($oauthConfig->toArray());
+        $server = new League\OAuth2\Server\Authorization(
+
+            new Oauth2\Server\Storage\Pdo\Mysql\Client($oauthdb),
+            new Oauth2\Server\Storage\Pdo\Mysql\Session($oauthdb),
+            new Oauth2\Server\Storage\Pdo\Mysql\Scope($oauthdb)
+        );
+        # Not required as it called directly from original code
+        # $request = new \League\OAuth2\Server\Util\Request();
+
+        # add these 2 lines code if you want to use my own Request otherwise comment it
+        $request = new \Oauth2\Server\Storage\Pdo\Mysql\Request();
+        $server->setRequest($request);
+
+        $server->setAccessTokenTTL(86400);
+        $server->addGrantType(new League\OAuth2\Server\Grant\ClientCredentials());
+        return $server;
+    });
+
+
+
+    $app->get('/access', function () use ($app) {
+
+        try {
+            $params = $app->oauth2->getParam(array('client_id', 'client_secret'));
+            echo json_encode(
+                $app->oauth2
+                    ->getGrantType('client_credentials')
+                    ->completeFlow($params)
+            );
+
+        } catch (League\OAuth2\Server\Exception\ClientException $e) {
+            var_export($e);
+            echo $e->getMessage();
+        } catch (\Exception $e) {
+            echo $e->getTraceAsString();
+        }
+    });
+
+    // Boom, Run
+	$app->run();
+
+} catch(Exception $e) {
+	// Do Something I guess, return Server Error message
+	$app->response->setStatusCode(500, "Server Error");
+	$app->response->setContent($e->getMessage());
+	$app->response->send();
+}
